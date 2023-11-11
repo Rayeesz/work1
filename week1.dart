ed6f28b74538a2e43cc94f81598d821fc4524573
Rayeez <rayeesanwar6365@gmail.com>
2023-11-11 12:54:17 +0530

    adding files

---
+++ week1.dart
@@ -0,0 +1,151 @@
+import 'dart:math';
+import 'dart:io';
+void main(){
+  
+
+
+var number =  5;
+if (number %2==0){print("even");}
+else{print("odd");}
+
+// int number = int.parse(stdin.readLineSync());
+// print("The number is $number");
+
+
+
+// var a =25;
+// var b =10;
+// print(a+b);
+
+
+
+//  List numb =[1,1,2,3,5,8,13,21,34,55,89];
+
+//  var a = numb.where((element)=>element<=5);
+//   print(a);
+
+
+
+// int p =1000;
+// double r=10.5;
+// double n=2.5;
+
+// var sum = p*r*n;
+// print(sum/100);
+
+
+
+
+//  List numbe =[1,4,9,16,25,36,49,81,100];
+//      for(var i =1;i<=9;i++)
+//      {if(numbe[i] %2==0){print(numbe[i]);}
+    
+//      }
+
+
+
+// var sort=[20,10,50,30,40];
+//  sort.sort();
+ 
+//  print(sort.reversed);
+
+//  var number=1;
+//  for(int i=0;i<=2;i++){
+
+//     for(int j=0;j<=i;j++){
+//         stdout.write("${number++} ");
+//     }
+    
+//     print(" ");
+//  }
+
+
+
+
+    // var number = 5;
+    // for (var i=1;i<=10;i++)
+    // {print("$i X 5 ${i*number}");}
+
+    // String b=" deepak ";
+    // String a = b.trim();
+    // print(a);
+
+  
+
+
+
+
+
+//   print(generatePassword(12)); 
+// }
+
+// String generatePassword(int length) {
+//   const String chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()-_=+[]{}|;:,.<>?/';
+
+//   String password = '';
+//   Random random = Random();
+
+//   for (int i = 0; i < length; i++) {
+//     int randomIndex = random.nextInt(chars.length);
+//     password += chars[randomIndex];
+//   }
+
+//   return password;
+//   print(generatePassword(12)); 
+
+
+// var b = pow(5,3);
+// print(b);
+
+//assigment
+
+// var x = 3 ;
+//  var y = 4 ;
+//   var z = 5;
+//   var temb = x;
+//   x = y;
+//   y = z;
+//   z = temb;
+
+// print(y);
+
+//   int n = 5;
+//   int result = 1;
+//   for (int i = 1; i <= n; i++) {
+//     result *=i;
+       
+  
+//   }
+//   print(result);
+
+
+
+
+// double calculateCircleArea(double radius) {
+  
+//   double area = pi * radius * radius;
+//   return area;
+// }
+
+// void main() {
+  
+//   double radius = double.parse(stdin.readLineSync()!);
+//   double areaOfCircle = calculateCircleArea(radius);
+//   print(" $radius is: $areaOfCircle");
+  
+// }
+
+// int width=5;
+// int height=10;
+// int area=width*height;
+// print("Area of the rectangle $area");
+
+
+// int a=2;
+// int area=a*a;
+// print("Area of the squre $area")
+
+
+}
+
+
